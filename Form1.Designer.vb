4a1206491d00a44f550ee4681514b43ff8c4422c
MAY 06 <MAY 06@LAB2-33>
2023-09-12 09:06:29 +0700

    Add project files.

---
+++ Form1.Designer.vb
@@ -0,0 +1,218 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.Label5 = New System.Windows.Forms.Label()
+        Me.Label6 = New System.Windows.Forms.Label()
+        Me.txta = New System.Windows.Forms.TextBox()
+        Me.txtb = New System.Windows.Forms.TextBox()
+        Me.txtc = New System.Windows.Forms.TextBox()
+        Me.txtx1 = New System.Windows.Forms.TextBox()
+        Me.txtx2 = New System.Windows.Forms.TextBox()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.Button2 = New System.Windows.Forms.Button()
+        Me.Label7 = New System.Windows.Forms.Label()
+        Me.Label8 = New System.Windows.Forms.Label()
+        Me.SuspendLayout()
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Font = New System.Drawing.Font("Microsoft Sans Serif", 30.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label1.Location = New System.Drawing.Point(175, 9)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(470, 47)
+        Me.Label1.TabIndex = 0
+        Me.Label1.Text = "Giải Phương Trình Bậc 2"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label2.Location = New System.Drawing.Point(47, 110)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(116, 20)
+        Me.Label2.TabIndex = 1
+        Me.Label2.Text = "Nhập Hệ Số A"
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label3.Location = New System.Drawing.Point(47, 152)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(117, 20)
+        Me.Label3.TabIndex = 1
+        Me.Label3.Text = "Nhập Hệ Số B"
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label4.Location = New System.Drawing.Point(47, 193)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(117, 20)
+        Me.Label4.TabIndex = 1
+        Me.Label4.Text = "Nhập Hệ Số C"
+        '
+        'Label5
+        '
+        Me.Label5.AutoSize = True
+        Me.Label5.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label5.Location = New System.Drawing.Point(60, 233)
+        Me.Label5.Name = "Label5"
+        Me.Label5.Size = New System.Drawing.Size(88, 20)
+        Me.Label5.TabIndex = 1
+        Me.Label5.Text = "Nghiệm x1"
+        '
+        'Label6
+        '
+        Me.Label6.AutoSize = True
+        Me.Label6.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label6.Location = New System.Drawing.Point(60, 272)
+        Me.Label6.Name = "Label6"
+        Me.Label6.Size = New System.Drawing.Size(88, 20)
+        Me.Label6.TabIndex = 1
+        Me.Label6.Text = "Nghiệm x2"
+        '
+        'txta
+        '
+        Me.txta.Location = New System.Drawing.Point(200, 110)
+        Me.txta.Name = "txta"
+        Me.txta.Size = New System.Drawing.Size(234, 20)
+        Me.txta.TabIndex = 2
+        '
+        'txtb
+        '
+        Me.txtb.Location = New System.Drawing.Point(200, 152)
+        Me.txtb.Name = "txtb"
+        Me.txtb.Size = New System.Drawing.Size(234, 20)
+        Me.txtb.TabIndex = 2
+        '
+        'txtc
+        '
+        Me.txtc.Location = New System.Drawing.Point(200, 193)
+        Me.txtc.Name = "txtc"
+        Me.txtc.Size = New System.Drawing.Size(234, 20)
+        Me.txtc.TabIndex = 2
+        '
+        'txtx1
+        '
+        Me.txtx1.Location = New System.Drawing.Point(200, 233)
+        Me.txtx1.Name = "txtx1"
+        Me.txtx1.Size = New System.Drawing.Size(234, 20)
+        Me.txtx1.TabIndex = 2
+        '
+        'txtx2
+        '
+        Me.txtx2.Location = New System.Drawing.Point(200, 272)
+        Me.txtx2.Name = "txtx2"
+        Me.txtx2.Size = New System.Drawing.Size(234, 20)
+        Me.txtx2.TabIndex = 2
+        '
+        'Button1
+        '
+        Me.Button1.BackColor = System.Drawing.SystemColors.ActiveCaption
+        Me.Button1.Location = New System.Drawing.Point(141, 361)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(100, 28)
+        Me.Button1.TabIndex = 3
+        Me.Button1.Text = "Giải Phương Trình"
+        Me.Button1.UseVisualStyleBackColor = False
+        '
+        'Button2
+        '
+        Me.Button2.BackColor = System.Drawing.SystemColors.ControlDark
+        Me.Button2.Location = New System.Drawing.Point(381, 361)
+        Me.Button2.Name = "Button2"
+        Me.Button2.Size = New System.Drawing.Size(100, 28)
+        Me.Button2.TabIndex = 3
+        Me.Button2.Text = "Thoát"
+        Me.Button2.UseVisualStyleBackColor = False
+        '
+        'Label7
+        '
+        Me.Label7.AutoSize = True
+        Me.Label7.Font = New System.Drawing.Font("Microsoft Sans Serif", 13.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label7.Location = New System.Drawing.Point(65, 314)
+        Me.Label7.Name = "Label7"
+        Me.Label7.Size = New System.Drawing.Size(64, 22)
+        Me.Label7.TabIndex = 4
+        Me.Label7.Text = "Label7"
+        '
+        'Label8
+        '
+        Me.Label8.AutoSize = True
+        Me.Label8.Font = New System.Drawing.Font("Microsoft Sans Serif", 13.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label8.Location = New System.Drawing.Point(202, 314)
+        Me.Label8.Name = "Label8"
+        Me.Label8.Size = New System.Drawing.Size(0, 22)
+        Me.Label8.TabIndex = 4
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(800, 450)
+        Me.Controls.Add(Me.Label8)
+        Me.Controls.Add(Me.Label7)
+        Me.Controls.Add(Me.Button2)
+        Me.Controls.Add(Me.Button1)
+        Me.Controls.Add(Me.txtx2)
+        Me.Controls.Add(Me.txtx1)
+        Me.Controls.Add(Me.txtc)
+        Me.Controls.Add(Me.txtb)
+        Me.Controls.Add(Me.txta)
+        Me.Controls.Add(Me.Label6)
+        Me.Controls.Add(Me.Label5)
+        Me.Controls.Add(Me.Label4)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents Label3 As Label
+    Friend WithEvents Label4 As Label
+    Friend WithEvents Label5 As Label
+    Friend WithEvents Label6 As Label
+    Friend WithEvents txta As TextBox
+    Friend WithEvents txtb As TextBox
+    Friend WithEvents txtc As TextBox
+    Friend WithEvents txtx1 As TextBox
+    Friend WithEvents txtx2 As TextBox
+    Friend WithEvents Button1 As Button
+    Friend WithEvents Button2 As Button
+    Friend WithEvents Label7 As Label
+    Friend WithEvents Label8 As Label
+End Class
---
+++ Form1.vb
@@ -0,0 +1,30 @@
+﻿Public Class Form1
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        Dim a As Integer
+        Dim b As Integer
+        Dim c As Integer
+        Dim x1 As Single
+        Dim x2 As Single
+        a = Integer.Parse(txta.Text)
+        b = Integer.Parse(txtb.Text)
+        c = Integer.Parse(txtc.Text)
+        If a = 0 Then
+            x1 = -c / b
+        Else
+            Dim delta As Integer
+            delta = b * b - 4 * a * c
+            If delta < 0 Then
+                MessageBox.Show("PT Vô Nghiệm")
+            ElseIf delta = 0 Then
+                x1 = -b / 2 * a
+            Else
+                x1 = (-b - Math.Sqrt(delta)) / (2 * a)
+                x2 = -b + Math.Sqrt(delta) / (2 * a)
+            End If
+
+        End If
+        txtx1.Text = x1
+        txtx2.Text = x2
+
+    End Sub
+End Class
